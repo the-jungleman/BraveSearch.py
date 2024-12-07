155765052e98aba37956b65f410abe229bcfdfad
the-jungleman <the-jungleman@proton.me>
2024-12-07 14:32:06 -0300

    finished

--- Tela.py
+++ Tela.py
@@ -1,30 +1,28 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
-import json,requests
-from tkhtmlview import HtmlFrame
+import json
+import requests
+from tkinterweb import HtmlFrame  
 from ConsumeAPI import ConsumeAPI
 
 class Tela:
     def __init__(self, root):
         self.root = root
         self.root.title("BraveSearch.py")
-        self.root.geometry("800x600")  # Ajuste do tamanho da janela
+        self.root.geometry("1368x768")
 
         self.query_label = tk.Label(self.root, text="Pesquisar")
         self.query_label.pack(pady=5)
 
-        # Caixa de pesquisa
         self.query_entry = tk.Entry(self.root, width=40)
         self.query_entry.pack(pady=5)
 
         self.search_button = tk.Button(self.root, text="Buscar", command=self.command_search_button)
         self.search_button.pack(pady=10)
 
-        # Frame para armazenar os botões de resultados
         self.results_frame = tk.Frame(self.root)
         self.results_frame.pack(pady=10)
 
-        # Frame para exibir o conteúdo HTML da página
         self.html_frame = HtmlFrame(self.root, width=750, height=400)
         self.html_frame.pack(pady=10)
 
@@ -33,9 +31,12 @@ class Tela:
     def search(self):
         search = self.query_entry.get()
 
-        # Limpa os resultados anteriores
         for widget in self.results_frame.winfo_children():
             widget.destroy()
+
+        self.results_frame.pack_forget()
+
+        self.html_frame.pack_forget()
 
         api = ConsumeAPI()
         data = api.consume_api(search)
@@ -43,6 +44,9 @@ class Tela:
         if data and 'web' in data:
             for result in data['web']['results']:
                 self.insert_button(result)
+            
+            self.results_frame.pack(pady=10)
+
         else:
             messagebox.showerror("Error", "Nenhum resultado encontrado")
 
@@ -51,23 +55,24 @@ class Tela:
             title = result['title']
             url = result['url']
 
-            # Criando o botão com o título
             button = tk.Button(self.results_frame, text=title, command=lambda: self.open_url(url))
             button.pack(fill='x', pady=5)
 
-            # Armazenando os dados na lista
             self.data_list.append({'title': title, 'url': url})
 
         except KeyError as e:
             print(f"Chave {e} não encontrada")
 
     def open_url(self, url):
-        # Fazendo a requisição da página para obter o conteúdo
         try:
             response = requests.get(url)
             if response.status_code == 200:
                 html_content = response.text
-                self.html_frame.set_content(html_content)  # Exibe o conteúdo HTML no Tkinter
+                self.html_frame.load_html(html_content)  
+                self.results_frame.pack_forget()
+
+                self.html_frame.pack(pady=10)
+
             else:
                 messagebox.showerror("Erro", f"Falha ao carregar a página: {response.status_code}")
         except requests.exceptions.RequestException as e:
